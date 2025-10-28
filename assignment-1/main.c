bfe2645d7d4467f3cfbe57a28641f58ba9675fba
Starbuck7410 <starbuck7410@gmail.com>
2025-10-28 18:56:28 +0200

    changed things

--- assignment-1/main.c
+++ assignment-1/main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-int main()
-{
-    int number;
-    printf("Rami is a badass\n");
-    scanf("%d,&number");
+int main(){
+    int number = 4;
+
+    printf("Rami is a badass and lives in %llx\n", &number);
     return 0;
 }
