714c72872f784859d6b9ce95a28b4cbb678371ea
Rami <ramipe1809@gmail.com>
2025-11-04 18:38:40 +0200

    added homework 1 before check

--- assignment-1/ex1.c
+++ assignment-1/ex1.c
@@ -1,6 +1,70 @@
 #include <stdio.h>
 
-int main(){
-    printf("Asd\n");
+int main() {
+
+    int number, base, num_digits, sum_digits = 0, temp, abs_value;
+    char sign;
+
+    printf("Please enter a number (integer) and a base:\n");
+    scanf("%d %d",&number,&base);
+
+    while(base != 8 && base != 10 && base != 16) {
+
+        printf("Invalid base %d\n",base);
+        printf("Please enter a valid base [8,10,16]:\n");
+        scanf("%d",&base);
+    }
+
+    printf("Here is some information about the given numbers: %d\n",number);
+
+    if(number > 0)
+    sign = '+';
+    
+    else if (number == 0)
+    sign = '0';
+
+    else 
+    sign = '-';
+
+    printf("Its sign is: %c",sign);
+
+    temp = number;
+
+    if(sign == '-')
+    abs_value = -temp;
+
+    else 
+    abs_value = temp;
+
+    printf("Decimal representation of the absolute value: %d\n",abs_value);
+
+    if(temp == 0) 
+        num_digits = 1;
+    
+    while(temp != 0) {
+
+        num_digits++;
+        sum_digits += temp % 10;
+        temp /= 10;
+    }
+
+    printf("The number of digits is: $d\n",num_digits);
+    printf("The sum of digits is: %d\n",sum_digits);
+
+    switch(base) {
+
+    case 8:
+        printf("0d%d = %o\n",abs_value,abs_value);
+        break;
+
+    case 16:
+        printf("0d%d = %#x\n",abs_value,abs_value);
+        break;
+
+    case 10:
+        printf("Base 10 representation was already computed above.\n");
+        break;
+    }
+    
     return 0;
 }
--- assignment-1/ex2.c
+++ assignment-1/ex2.c
@@ -1,6 +1,65 @@
 #include <stdio.h>
 
-int main(){
-    printf("dsA\n");
+int main() {
+
+    int grade, failed_attemps = 0;
+
+    printf("Please enter the exam score (0-100):\n");
+    scanf("%d",&grade);
+
+    while(grade > 100 || grade < 0) {
+        
+        failed_attemps++;
+
+        if(failed_attemps == 3) {
+
+            printf("The user has inserted 3 consecutive times an invalid score.\n");
+            printf("Exiting the program!\n");
+            return 0;
+        }
+
+        printf("Invalid exam score: %d\n",grade);
+        printf("Please enter a new exam score in the range 0-100:\n");
+        scanf("%d",&grade);
+    }
+
+    switch(grade / 10) {
+
+        case 9:
+        case 10:
+            printf("Grade: A\n");
+            printf("Excellent work!\n");
+            break;
+
+        case 8:
+            printf("Grade: B\n");
+            printf("Well done.\n");
+            break;
+            
+        case 7:
+            printf("Grade: C\n");    
+            printf("Not bad! You can do better..\n");
+            break;
+
+        case 6:
+            printf("Grade: D\n");
+            printf("You passed, but there is room for improvement.\n");
+            break;
+            
+        case 5:
+            printf("Grade: E\n");
+            printf("Barely passed - you should work harder next time.\n");
+            break;
+
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 0:
+            printf("Grade: F\n");
+            printf("You Failed - you need to review the material.\n");
+            break;
+    }
+
     return 0;
 }
