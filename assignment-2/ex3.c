d6605715921d5ef6091f2a008da6c02626944b70
Starbuck7410 <starbuck7410@gmail.com>
2025-11-15 23:01:46 +0200

    added test 5

--- assignment-2/ex3.c
+++ assignment-2/ex3.c
@@ -1,50 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_TARGET (9 * 9 * 9)
+#define MIN_TARGET 0
+#define MAX_INPUT 999
+#define MIN_INPUT 000
+#define MAX_LINE 1000
 
-#define MAX_TARGET (9*9*9)
-#define MIN_TARGET (-9*9)
-#define MAX_INPUT  999
-#define MIN_INPUT  000
-
-
-int apply_op(int a, int b, char op) {
-    if (op == '+') return a + b;
-    if (op == '-') return a - b;
-    return a * b;  // op == '*'
+int apply_op(int a, int b, char op)
+{
+    if (op == '+')
+        return a + b;
+    if (op == '-')
+        return a - b;
+    return a * b; // op == '*'
 }
 
-int evaluate(int d1, int d2, int d3, char op1, char op2) {
+int evaluate(int d1, int d2, int d3, char op1, char op2)
+{
     // Check for multiplications
-    if (op1 == '*') {
+    if (op1 == '*'){
         int left = d1 * d2;
         return apply_op(left, d3, op2);
-    } else if (op2 == '*') {
+    }
+    else if (op2 == '*'){
         int right = d2 * d3;
         return apply_op(d1, right, op1);
-    } else {
+    }
+    else{
         int tmp = apply_op(d1, d2, op1);
         return apply_op(tmp, d3, op2);
     }
 }
 
-int main() {
+int main(void){
+    char num_text[MAX_LINE];
     int num, target;
     printf("Enter a 3-digit integer:\n");
-    if (scanf("%d", &num) != 1) {
-        printf("Invalid input. Must be a 3-digit number.\n");
+    
+    if (scanf("%s", num_text) != 1){
+        printf("Invalid input.\n");
         return 0;
     }
+    num = atoi(num_text);
 
-    if (num < MIN_INPUT || num > MAX_INPUT) {
+
+    if (num < MIN_INPUT || num > MAX_INPUT || strlen(num_text) < 3){
         printf("Invalid input. Must be a 3-digit number.\n");
         return 0;
     }
 
     printf("Enter target value:\n");
-    if (scanf("%d", &target) != 1) {
+    if (scanf("%d", &target) != 1){
         printf("Invalid target.\n");
         return 0;
     }
-    if (target < MIN_TARGET || target > MAX_TARGET) {
+    if (target < MIN_TARGET || target > MAX_TARGET){
         printf("Invalid target. target %d is outside of range.\n", target);
         return 0;
     }
@@ -61,21 +72,20 @@ int main() {
         {d1, d0, d2},
         {d1, d2, d0},
         {d2, d0, d1},
-        {d2, d1, d0}
-    };
+        {d2, d1, d0}};
 
     char ops[3] = {'+', '-', '*'};
     int count = 0;
 
-    for (int p = 0; p < 6; p++) {
+    for (int p = 0; p < 6; p++){
         int a = perms[p][0];
         int b = perms[p][1];
         int c = perms[p][2];
 
         // all operator combinations
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (evaluate(a, b, c, ops[i], ops[j]) == target) {
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                if (evaluate(a, b, c, ops[i], ops[j]) == target){
                     count++;
                 }
             }
