6ee1c32e8617bf9bb1ac7b0a9c219e4bd2adbb1b
Rami <ramipe1809@gmail.com>
2025-12-16 14:38:22 +0200

    finished the database, didn't check any validation tho

--- assignment-4/database.c
+++ assignment-4/database.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include "database.h"
 
+#define NOT_FOUND -1
+
 void create_database(Database *db, int initial_capacity) {
     
     if(db == NULL) {
@@ -23,7 +25,6 @@ void create_database(Database *db, int initial_capacity) {
     db -> count = 0;
 }
 
-
 void db_add_song(Database *db, Song *s) {
 
     if(db == NULL) {
@@ -38,33 +39,83 @@ void db_add_song(Database *db, Song *s) {
         exit(1);
     }
 
-    if((db -> count) < (db -> capacity)) {
+    if((db -> count) == (db -> capacity)) { 
 
-        *((db -> songs) + (db -> count))  = s; // going to the end of the array of songs and setting there new song
-        (db -> count)++;
-    }
-
-    else { // the database is full
-
-        (db -> capacity) *= 2;
-        (db -> songs) = (Song **) realloc((db -> songs) ,(db -> capacity) * sizeof(Song *));
-
-    }
+        int new_capacity = (db -> capacity) * 2;
+        Song **temp = (Song **) realloc((db -> songs) ,new_capacity * sizeof(Song *));
     
-    printf("ERROR: Failed to expand database.\n");
+        if (temp == NULL)
+        {
+            printf("ERROR: Failed to expand database.\n");
+            exit(1);
+        }       
+
+        (db -> songs) = temp;
+        db -> capacity = new_capacity;
+    }
+
+    *((db -> songs) + (db -> count))  = s; // going to the end of the array of songs and setting there new song
+    (db -> count)++;
 }
 
 void db_remove_song(Database *db, const char *title) {
-    printf("ERROR: NULL Database pointer.\n");
+    
+    if(db == NULL) {
+        printf("ERROR: NULL Database pointer.\n");
+        exit(1);
+    }
 
-    printf("ERROR: NULL title.\n");
+    if(title == NULL) {
+        printf("ERROR: NULL title.\n");
+        exit(1);
+    }
 
-    printf("ERROR: Song not found in database.\n");
+    int found_removal = NOT_FOUND; // didn't find yet
 
+    for (int i = 0; i < (db -> count) ; i++) {
+        
+        Song *temp = (db -> songs)[i];
+
+        if((strcmp(temp -> title, title) == 0) && found_removal == NOT_FOUND) {
+
+            found_removal = i;
+            
+            free(temp -> artist);
+            free(temp -> title);
+            free(temp);
+
+            break;
+        } 
+    }
+
+    if(found_removal == NOT_FOUND) { // didn't find the song
+
+        printf("ERROR: Song not found in database.\n");
+        return;
+    }
+
+    for (int i = found_removal; i < (db -> count) - 1; i++) // shifting the entries
+    {
+        (db -> songs)[i] = (db -> songs)[i + 1];
+    }
+    
+    (db -> count)--;
 }
 
 void free_db(Database *db) {
 
-    printf("ERROR: NULL Database pointer.\n");
+    if(db == NULL) {
 
+        printf("ERROR: NULL Database pointer.\n");
+        exit(1);
+    }
+
+    for(int i = 0; i < (db -> count); i++) { // we have only count numbers of songs in the array in size capacity
+
+        free((db -> songs)[i] -> title);
+        free((db -> songs)[i] -> artist);
+        free((db -> songs)[i]);
+    }
+
+    free(db -> songs);
 }
