b3a16fd9fd854bcfd687c6766097f337f8ca0ac6
Rami <ramipe1809@gmail.com>
2025-12-26 13:06:48 +0200

    finished ex1

--- assignment-5/ex1.c
+++ assignment-5/ex1.c
@@ -2,6 +2,18 @@
 
 unsigned int largest_power_of_2(unsigned int n) {
   // TODO
+
+  if (n == 0) // We are handling the case n = 0 eventhough in the PDF we are told to assume it's bigger
+    return 0;
+    
+  unsigned int largest_power_which_smaller = 1;
+
+  while(largest_power_which_smaller <= (n >> 1)) {
+
+    largest_power_which_smaller = largest_power_which_smaller << 1;
+  }
+  
+  return largest_power_which_smaller;
 }
 
 int main() {
