f197d24a608790faad70b2d8fa941624f7d6d0d4
Rami <ramipe1809@gmail.com>
2025-12-26 21:34:54 +0200

    finished ex3 but didn't check yet

--- assignment-5/ex3.c
+++ assignment-5/ex3.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#define BITS_IN_BYTE 8 // one byte contains 8 bits
+#define VALUE_OPTIONS_IN_BYTE 256 // 8 bits so it can have 2^8 different values
+
 typedef struct {
   unsigned long int cnt_1;
   unsigned long int cnt_0;
@@ -16,10 +19,56 @@ static void print_stats(stats_t stats) {
   printf("Middle byte value: 0x%X\n", stats.byte_mdl);
 }
 
+
 static stats_t compute_stats(FILE *file) {
   stats_t stats = {};
+  
+  int c;
+  unsigned int num_of_bytes = 0;
+  unsigned int one_occurences = 0;
+  unsigned int zero_occurences = 0;
+
+  int byte_occurences[VALUE_OPTIONS_IN_BYTE] = {0};
+  
 
   // TODO: compute the statistics
+
+  if(file == NULL) return stats; // check what to do -- cause it feels off
+
+  while((c = fgetc(file)) != EOF) {
+
+    byte_occurences[c]++;
+    num_of_bytes++;
+    
+    unsigned char byte = (unsigned char)c;
+    for (int i = 0; i < BITS_IN_BYTE; i++) {
+  
+      if((byte >> i) & 1)
+        one_occurences++;
+
+      else
+        zero_occurences++;  
+    }
+  }
+
+  int max = 0;
+  unsigned char max_freq_byte = 0;
+  for (int i = 0; i < VALUE_OPTIONS_IN_BYTE; i++)
+  {
+    if(byte_occurences[i] > max) {
+      max = byte_occurences[i];
+      max_freq_byte = (unsigned char)i;
+    }
+  }
+  
+
+  stats.cnt_bytes = num_of_bytes;
+  stats.cnt_1 = one_occurences;
+  stats.cnt_0 = zero_occurences;
+  stats.byte_freq = max_freq_byte;
+
+  fseek(file,num_of_bytes / 2,SEEK_SET);
+  stats.byte_mdl = fgetc(file);
 
   return stats;
 }
@@ -34,9 +83,15 @@ int main(int argc, char *argv[]) {
   }
 
   // TODO: compute stats from file
+  FILE *file;
 
+  if((file = fopen(argv[1],"rb")) == NULL)
+    return 1;
+
+  stats = compute_stats(file);  
   // print the results
   print_stats(stats);
 
+  fclose(file);
   return 0;
 }
