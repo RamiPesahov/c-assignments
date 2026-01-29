0b407948477df9c4b63a0dc9f862432f111505b8
Rami <ramipe1809@gmail.com>
2026-01-29 14:55:12 +0200

    attached my previous solution to this skeleton

--- assignment-6/ex3.c
+++ assignment-6/ex3.c
@@ -2,7 +2,6 @@
 #include <stdint.h>
 #include "fixed_point.h"
 
-
 int main(int argc, char **argv) {
     if (argc != 6) {
         printf("Usage: %s <x_raw> <a_raw> <b_raw> <c_raw> <q>\n", argv[0]);
@@ -11,6 +10,21 @@ int main(int argc, char **argv) {
     }
 
     // TODO
+    int int_x, int_a, int_b, int_c, int_q;
+    
+    sscanf(argv[1], "%d", &int_x);
+    sscanf(argv[2], "%d", &int_a);
+    sscanf(argv[3], "%d", &int_b);
+    sscanf(argv[4], "%d", &int_c);
+    sscanf(argv[5], "%d", &int_q);
+
+    int16_t x = (int16_t)int_x;
+    int16_t a = (int16_t)int_a;
+    int16_t b = (int16_t)int_b;
+    int16_t c = (int16_t)int_c;
+    int16_t q = (int16_t)int_q;
+
+    eval_poly_ax2_minus_bx_plus_c_fixed(x, a, b, c, q);
 
     return 0;
 }
--- assignment-6/fixed_point.c
+++ assignment-6/fixed_point.c
@@ -1,22 +1,36 @@
 #include "fixed_point.h"
 #include <stdio.h>
 
+#define CONVERT_TO_DECIMAL 1000000
+
 void print_fixed(int16_t raw, int16_t q) {
     // TODO: Print up to 6 decimal places (with truncation)
+    
+    int16_t mask = (1 << q) - 1;
+    uint16_t fraction = raw & mask;
+    uint32_t decimal = ((uint32_t)(fraction * CONVERT_TO_DECIMAL)) >> q;
+
+    printf("%d.%06u",(raw >> q), decimal);
 }
 
 int16_t add_fixed(int16_t a, int16_t b) {
     // TODO: Implement addition
+     
+    return (a + b);
 }
 
 int16_t subtract_fixed(int16_t a, int16_t b) {
     // TODO: Implement subtraction
+
+    return (a - b);
 }
 
 int16_t multiply_fixed(int16_t a, int16_t b, int16_t q) {
     // TODO: Implement fixed-point multiply:
        //- Use a wider type for intermediate multiplication (e.g., int32_t or int64_t).
-   
+
+       int64_t intermidiate = ((int64_t)a * (int64_t)b);
+       return (int16_t) (intermidiate >> q);
 }
 
 void eval_poly_ax2_minus_bx_plus_c_fixed(int16_t x, int16_t a, int16_t b, int16_t c, int16_t q) {
@@ -24,6 +38,11 @@ void eval_poly_ax2_minus_bx_plus_c_fixed(int16_t x, int16_t a, int16_t b, int16_
        Evaluate: y = a*x^2 - b*x + c
     */
 
+    int16_t x2 = multiply_fixed(x, x, q);
+    int16_t ax2 = multiply_fixed(a, x2, q);
+    int16_t bx = multiply_fixed(b, x, q);
+    int16_t y = add_fixed(subtract_fixed(ax2, bx), c);
+   
     printf("the polynomial output for a=");
     print_fixed(a, q);
     printf(", b=");
@@ -33,5 +52,5 @@ void eval_poly_ax2_minus_bx_plus_c_fixed(int16_t x, int16_t a, int16_t b, int16_
     printf(" is ");
     /* TODO: print y once computed */
     /* print_fixed(y, q); */
-
+    print_fixed(y, q);
 }
